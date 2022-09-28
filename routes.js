92d082c414fe22c917f9462160bd41550d3da86b
jvholanda2 <jvholanda18@gmail.com>
2022-09-28 01:57:45 -0300

    fix: Redirecionamento dashboard

--- routes.js
+++ routes.js
@@ -2,13 +2,13 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/',function(req,res){
-    return res.redirect("/instructors")
+    return res.redirect("/login")
 })
 routes.get('/login',function(req,res){
     return res.render("login/login")
 })
-routes.get('/members',function(req,res){
-    return res.send("members")
+routes.get('/dashboard',function(req,res){
+    return res.render("dashboard/dashboard")
 })
 
 module.exports = routes
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
-const routes = require('./routes')
+const express = require('express');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
-const server = express()
+const server = express();
 
-server.use(express.static('public'))
-server.use(routes)
+server.use(express.static('public'));
+server.use(routes);
 
-server.set('view engine', 'njk')
+server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
     express: server,
     autoescape: false,
     noCache: true
-})
+});
 
 server.listen(5000, function() {
-    console.log('Tá rodando')
-})
+    console.log('Tá rodando');
+});
 
